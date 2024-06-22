a7467d61090b7de0e9a2a58f2b912b3003fe5dba
Tregubov Maksim <tregubov-m@inbox.ru>
2024-06-22 14:02:26 +0300

    add turbo to borrowing

--- app/controllers/borrowings_controller.rb
+++ app/controllers/borrowings_controller.rb
@@ -14,16 +14,16 @@ class BorrowingsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    create_borrowing_and_update_book
+    @book = create_borrowing_and_update_book
 
     respond_to do |format|
       format.html { redirect_to book_path(@book), flash: { success: t('.success') } }
       format.turbo_stream do
         method = case params[:context]
                  when 'all_books'
-                   turbo_stream.update(@borrowing.book, partial: 'books/book_all', locals: { book: @borrowing.book })
+                   turbo_stream.update(@borrowing.book, partial: 'books/book_all', locals: { book: @book })
                  when 'single_book'
-                   turbo_stream.update(@borrowing.book, partial: 'books/book', locals: { book: @borrowing.book })
+                   turbo_stream.update(@borrowing.book, partial: 'books/book', locals: { book: @book })
                  end
 
         render turbo_stream: [method,
@@ -38,7 +38,7 @@ class BorrowingsController < ApplicationController
 
   def update
     @borrowing = Borrowing.includes(:book).find(params[:id])
-    update_borrowing_and_update_book
+    @borrowing = update_borrowing_and_update_book
 
     respond_to do |format|
       format.html { redirect_to book_path(@borrowing.book), flash: { success: t('.success') } }
@@ -48,6 +48,8 @@ class BorrowingsController < ApplicationController
                    turbo_stream.update(@borrowing.book, partial: 'books/book_all', locals: { book: @borrowing.book })
                  when 'single_book'
                    turbo_stream.update(@borrowing.book, partial: 'books/book', locals: { book: @borrowing.book })
+                 when 'taked_books'
+                   turbo_stream.remove("borrowing_#{@borrowing.book.id}")
                  end
 
         render turbo_stream: [method,
@@ -67,6 +69,7 @@ class BorrowingsController < ApplicationController
       @borrowing = @book.borrowings.create!(user: current_user, borrowed_at: Time.zone.now)
       @book.decrement(:available_books).save!
     end
+    @book
   end
 
   def update_borrowing_and_update_book
@@ -74,5 +77,6 @@ class BorrowingsController < ApplicationController
       @borrowing.update!(returned_at: Time.zone.now)
       @borrowing.book.increment(:available_books).save!
     end
+    @borrowing
   end
 end
